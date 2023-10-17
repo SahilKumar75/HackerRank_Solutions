6cd78d0c2fdc45cfca0f89b0e8d19a84a9b1a5ee
Sahil Kumar Singh <60318530+SahilKumar75@users.noreply.github.com>
2023-10-17 11:46:25 +0530

    Update viral_advertising.cpp

--- viral_advertising.cpp
+++ viral_advertising.cpp
@@ -2,22 +2,14 @@
 using namespace std;
 
 int main() {
-    int n, c = 0, s = 5, sum = 0;
-    cout << "Enter the number of iterations (n): ";
+    int n, sum = 0;
     cin >> n;
 
-    for (int i = 0; i < n; i++) {
-        // Calculate the number of likes on each day and add it to the total sum.
-        int dailyLikes = s / 2;  // Integer division by 2
-        sum += dailyLikes;
-
-        // Update the total shares for the next day.
-        s = dailyLikes * 3;  // Multiply the dailyLikes by 3 for the next day.
-
-        // Optionally, you can print the daily likes for each day.
-        cout << "Day " << i + 1 << ": " << dailyLikes << " likes" << endl;
+    for (int i = 0, s = 5; i < n; i++) {
+        sum += s / 2;
+        s = (s / 2) * 3;
     }
 
-    cout << "Total likes after " << n << " days: " << sum << endl;
+    cout << sum << endl;
     return 0;
 }
