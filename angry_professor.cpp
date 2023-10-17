10267d580ead60de1df8c2f8f405df2c998abf31
Sahil Kumar Singh <60318530+SahilKumar75@users.noreply.github.com>
2023-10-17 11:32:34 +0530

    Update angry_professor.cpp

--- angry_professor.cpp
+++ angry_professor.cpp
@@ -8,5 +8,23 @@ int main(){
     int i, j;
  
     scanf("%d", &T);
+for (i = 0; i < T; i++){
+        scanf("%d %d", &N, &K);
+        for (j = 0; j < N; j++){
+            scanf("%d", &a[j]);
+            
+            if (a[j] <= 0){
+                K--;
+            }
+        }
+ 
+        if (K <= 0){
+            printf("NO\n");
+        }
+        else{
+            printf("YES\n");
+        }
+    }
+}
  
     
