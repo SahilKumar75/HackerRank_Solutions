759675b4f7ea985ea16419841003a737eab3ff52
Sahil Kumar Singh <60318530+SahilKumar75@users.noreply.github.com>
2023-10-25 20:55:28 +0530

    Update minimum_distance.cpp

--- minimum_distance.cpp
+++ minimum_distance.cpp
@@ -10,4 +10,20 @@ int main() {
         cin >> arr[i];
     }
 
-                
+    int dist = INT_MAX;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < i; j++) {
+            if (arr[i] == arr[j]) {
+                dist = min(dist, abs(i - j));
+            }
+        }
+    }
+
+    if (dist == INT_MAX) {
+        dist = -1;
+    }
+
+    cout << dist << endl;
+
+    return 0;
+}
