ac6207e2d9f663378ec5a91beb4dee6768651223
Sahil Kumar Singh <60318530+SahilKumar75@users.noreply.github.com>
2023-10-25 21:43:13 +0530

    Update jumping_on_clouds.cpp

--- jumping_on_clouds.cpp
+++ jumping_on_clouds.cpp
@@ -7,3 +7,17 @@ int main(){
     for(int i=0;i<n;i++){
         cin>>a[i];
     }
+    int i=0;
+    while(i<n){
+        if(a[i+2]==0){
+            i+=2;c++;
+        }
+        else if(a[i+1]==0){
+            i+=1;c++;
+        }
+        if(i==n-1){
+            break;
+        }
+    }
+    cout<<c<<endl;
+}
