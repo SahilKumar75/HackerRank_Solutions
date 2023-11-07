615c2f72d156216b84f71c1e9a43cefede634d52
Sahil Kumar Singh <60318530+SahilKumar75@users.noreply.github.com>
2023-11-07 23:55:50 +0530

    Create the time in words.cpp

---
+++ the time in words.cpp
@@ -0,0 +1,28 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+ 
+string minutes[30] = {"one minute",
+                "two minutes",
+                "three minutes",
+                "four minutes",
+                "five minutes",
+                "six minutes",
+                "seven minutes",
+                "eight minutes",
+                "nine minutes",
+                "ten minutes",
+                "eleven minutes",
+                "twelve minutes",
+                "thirteen minutes",
+                "forteen minutes",
+                "quarter",
+                "sixteen minutes",
+                "seventeen minutes",
+                "eighteen minutes",
+                "nineteen minutes",
+ return 0;
+}
