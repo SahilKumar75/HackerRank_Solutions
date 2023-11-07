ccf4e4767f6a3725dfc4c4cb15b64d99468a07e7
Sahil Kumar Singh <60318530+SahilKumar75@users.noreply.github.com>
2023-11-07 23:15:20 +0530

    Update choclate feast.cpp

--- choclate feast.cpp
+++ choclate feast.cpp
@@ -3,3 +3,15 @@
 int main() {
     int T, N, C, M, ans = 0;
     std::cin >> T;
+    while (T--) {
+        std::cin >> N >> C >> M;
+        int tmp = ans = N / C;
+        while (tmp >= M) {
+            ans++;
+            tmp -= M;
+            tmp++;
+        }
+        std::cout << ans << std::endl;
+    }
+    return 0;
+}
