26814c8319d8134b2d05268c37d24ecb9da1777d
Sahil Kumar Singh <60318530+SahilKumar75@users.noreply.github.com>
2023-11-07 23:16:49 +0530

    Update extra long factorial.cpp

--- extra long factorial.cpp
+++ extra long factorial.cpp
@@ -5,4 +5,23 @@ int main() {
     int num;
     int i, k = 0, j, carry = 0, arr[1000] = {1};
     cin >> num;
-  
+    
+    for (i = 1; i <= num; i++) {
+        for (j = 0; j <= k; j++) {
+            arr[j] = arr[j] * i + carry;
+            carry = arr[j] / 10;
+            arr[j] = arr[j] % 10;
+        }
+        
+        while (carry) {
+            k++;
+            arr[k] = carry % 10;
+            carry /= 10;
+        }
+    }
+    
+    for (i = k; i >= 0; i--)
+        cout << arr[i];
+    
+    return 0;
+}
