c57dc41bfa9bb67b80b466ae69fe0c3e4e455b1d
Sahil Kumar Singh <60318530+SahilKumar75@users.noreply.github.com>
2023-11-07 23:52:05 +0530

    Update encryption.cpp

--- encryption.cpp
+++ encryption.cpp
@@ -11,4 +11,28 @@ int main() {
     int lo = floor(sqrt(len)), hi = ceil(sqrt(len));
     int i, j;
     bool found = false;
-
+    
+    for (i = lo; i <= hi; i++) {
+        for (j = i; j <= hi; j++) {
+            if (i * j >= len) {
+                found = true;
+                break;
+            }
+        }
+        if (found) break;
+    }
+    
+    width = j;
+    height = i;
+    int index = 0;
+    
+    for (i = 0; i < width; i++) {
+        for (j = 0; j < height; j++) {
+            index = j * width + i;
+            if (index < len) cout << s[index];
+        }
+        cout << ' ';
+    }
+    
+    return 0;
+}
