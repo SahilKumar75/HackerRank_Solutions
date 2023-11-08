9eb1e43dcd73d0f205f83fd4cd359435fb48c085
Sahil Kumar Singh <60318530+SahilKumar75@users.noreply.github.com>
2023-11-08 12:39:10 +0530

    Update larry's array.cpp

--- larry's array.cpp
+++ larry's array.cpp
@@ -1,7 +1,29 @@
 #include <iostream>
 #include <vector>
 
-i% 2 == 0 ? "YES" : "NO") << std::endl;
+int main() {
+    int t;
+    std::cin >> t;
+    
+    for (int a0 = 0; a0 < t; a0++) {
+        int n;
+        std::cin >> n;
+        
+        std::vector<int> numbers(n);
+        for (int i = 0; i < n; i++) {
+            std::cin >> numbers[i];
+        }
+        
+        int number_of_inversions = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (numbers[i] > numbers[j]) {
+                    number_of_inversions++;
+                }
+            }
+        }
+        
+        std::cout << (number_of_inversions % 2 == 0 ? "YES" : "NO") << std::endl;
     }
     
     return 0;
