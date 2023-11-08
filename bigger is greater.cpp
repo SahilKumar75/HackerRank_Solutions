045683349b2a65d2026f8fc32faa4516173d45c0
Sahil Kumar Singh <60318530+SahilKumar75@users.noreply.github.com>
2023-11-08 12:35:23 +0530

    Update bigger is greater.cpp

--- bigger is greater.cpp
+++ bigger is greater.cpp
@@ -2,4 +2,16 @@
 #include <algorithm>
 #include <string>
 
-
+int main() {
+    int tc;
+    std::cin >> tc;
+    while (tc--) {
+        std::string s;
+        std::cin >> s;
+        if (std::next_permutation(s.begin(), s.end()))
+            std::cout << s << std::endl;
+        else
+            std::cout << "no answer" << std::endl;
+    }
+    return 0;
+}
